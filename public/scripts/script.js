463b52f11549b179ac7b6412898e936de9e54534
Mr Gold <individualog@gmail.com>
2022-11-18 17:23:13 +0000

    Implementation of search Bar complete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,4 @@
-
+// Import whole DB
 async function grabData() {
   const res = await fetch('http://localhost:3003/videos')
   // console.table(res)
@@ -17,13 +17,13 @@ function createVideoPreview(videos) {
     const { thumbnail, duration, authorPicture, title, author, stats } = video
 
     let videoPreview = document.createElement('div')
-    videoPreview.classList.add('video-preview')
+    videoPreview.classList.add('video-preview', 'pointer')
     videoPreview.innerHTML = `
        <div class="thumbnail-container">
        <img class="thumbnail" src="${(thumbnail != "N/A") ? thumbnail : "image_not_found.png"}">
        <div class="video-time">${duration}</div>
      </div>
-     <div class="video-info-grid">
+     <div class="video-info-grid | grid">
        <div class="channel-picture">
          <img class="profile-picture" src="${(authorPicture != "N/A") ? authorPicture : "image_not_found.png"}">
        </div>
@@ -45,4 +45,71 @@ function createVideoPreview(videos) {
   })
 }
 
+const searchBar = document.getElementById('search-bar')
+const searchButton = document.getElementById('search-button')
+
+searchButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  //   console.log('click')
+  // Click Button
+  // Redirect to function OR redirect to link?
+  // Link has to be built by a router
+  // Make a form post 
+  // express route accept post and 
+  // path is the search inofrmation as req.params
+  // use express url encoding middleware
+  // pass back search 
+  // serve asssets 
+  let searchInput = (searchBar.value).trim();
+  const URL = `http://localhost:3003/videos/search`;
+  const searchResult = await fetch(`${URL}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      title: searchInput
+    })
+  })
+  const results = await searchResult.json();
+  resultsPage(results)
+})
+
+const main = document.querySelector('main')
+
+async function resultsPage(results) {
+  videoGrid.innerHTML = "";
+  const resultGrid = document.createElement('section');
+  resultGrid.classList.add('result-grid', 'flex-column-center')
+  resultGrid.innerHTML = "";
+
+  results.forEach(result => {
+    const { thumbnail, duration, authorPicture, title, author, stats } = result
+
+    let resultPreview = document.createElement('div')
+    resultPreview.classList.add('result-preview', 'pointer', 'flex')
+    resultPreview.innerHTML = `
+       <div class="result-thumbnail-container">
+       <img class="thumbnail" src="${(thumbnail != "N/A") ? thumbnail : "image_not_found.png"}">
+       <div class="result-time">${duration}</div>
+     </div>
+     <div class="result-info-grid | grid">
+       <div class="channel-picture">
+         <img class="profile-picture" src="${(authorPicture != "N/A") ? authorPicture : "image_not_found.png"}">
+       </div>
+       <div class="result-info">
+         <p class="result-title | fw-semi-bold fs-700">
+           ${title}
+         </p>
+         <p class="result-author">
+           ${author}
+         </p>
+         <p class="result-stats">
+           ${stats}
+         </p>
+       </div>
+     </div>
+    `;
+    resultGrid.appendChild(resultPreview)
+  })
+  main.append(resultGrid)
+}
 
--- src/routes/video-route.js
+++ src/routes/video-route.js
@@ -1,12 +1,25 @@
 const { Router } = require("express");
 const { Video } = require("../models/index");
+const { Op } = require("sequelize");
 const videoRouter = Router();
 
-
-// Importing db
 videoRouter.get('/', async (req, res) => {
   const videos = await Video.findAll();
   res.send(videos);
 })
+
+// Importing db
+videoRouter.post('/search', async (req, res) => {
+  const { title } = req.body
+  const video = await Video.findAll({
+    where: {
+      title: {
+        [Op.substring]: title
+      }
+    }
+  });
+  res.send(video);
+})
+
 
 module.exports = videoRouter
